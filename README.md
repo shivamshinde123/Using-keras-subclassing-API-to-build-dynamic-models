5b34401ece209695230ea675ca4e6971fec0f8ab
Shivam Shinde <shivamshinde92722@gmail.com>
2022-06-02 00:33:21 +0530

    Created python file named modelCreation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Using-keras-subclassing-API-to-build-dynamic-models
+
+Model structure:
+
+<img src="ModelStructure\model_structure.png" alt="ModelStructure" width="600" height="600">
---
+++ model_creation/modelCreation.py
@@ -0,0 +1,10 @@
+from tensorflow import keras
+from data_loading.dataLoader import IndependentFeatureScaler
+
+"""
+Approach:
+    1. Subclass the Model class
+    2. Creating the needed layers in the class contructor
+    3. Performing needed computations in call method
+"""
+
